6eb24224b721a0dd340e52d185327dbadc7633fb
root <root@DESKTOP-87MMAU8.localdomain>
2018-06-27 11:33:09 +0000

    first commit

---
+++ src/testpackage/FIrefoxDriverDemo.java
@@ -0,0 +1,16 @@
+package testpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FIrefoxDriverDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Jacyzin/Documents/selenium/geckodriver.exe");
+		driver = new FirefoxDriver();
+		String baseURL = "http://www.uol.com.br";
+		driver.get(baseURL);
+	}
+	
+}
---
+++ src/testpackage/FirstTest.java
@@ -0,0 +1,10 @@
+package testpackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+	
+	WebDriver driver = new FirefoxDriver();
+
+}
