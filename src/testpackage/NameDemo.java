41384b00c8471a083ec65cb74bedda7f0efb7e0c
Henry Jacyzin <henryjacyzin@gmail.com>
2018-06-28 08:04:20 -0300

    Add example by name

---
+++ src/testpackage/NameDemo.java
@@ -0,0 +1,22 @@
+package testpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class NameDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		System.setProperty("webdriver.gecko.driver", "C:/Users/Jacyzin/Documents/selenium/drivers/geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		String baseURL = "http://google.com";
+		driver.get(baseURL);
+		
+		driver.findElement(By.id("lst-ib")).sendKeys("jacyzin");
+		driver.findElement(By.name("btnK")).click();
+		
+	}
+}
